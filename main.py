666a233d88d1e39bca209fe900654566b2fd1c16
vananh-ng <94485420+vananh-ng@users.noreply.github.com>
2023-05-30 20:46:14 +0200

    update some functions

--- main.py
+++ main.py
@@ -27,12 +27,48 @@ def get_token():
     json_result = json.loads(result.content)
     token = json_result["access_token"]
     return token
-token = get_token()
-print(token)
+
 
 
 def get_auth_header():
     return {
         "Authorization": "Bearer " + token
     }
+
+def search_for_artist(artist_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header()
+    data = {
+        "q": artist_name,
+        "type": "artist",
+        "limit": "1"
+    }
+    result = requests.get(url, headers=headers, params=data)
+    json_result = json.loads(result.content)["artists"]["items"]
+    if len(json_result) == 0:
+        print("No results found")
+        return None
+    return json_result[0]
+
+def get_artist_top_tracks(artist_id):
+    url = "https://api.spotify.com/v1/artists/" + artist_id + "/top-tracks"
+    headers = get_auth_header()
+    data = {
+        "country": "US"
+    }
+    result = requests.get(url, headers=headers, params=data)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
+token = get_token()
+#print(token)
+result = search_for_artist("The Beatles")
+print(result["name"])
+artist_id = result["id"]
+top_tracks = get_artist_top_tracks(artist_id)
+for track in top_tracks:
+    print(track["name"])
+    print(track["preview_url"])
+    print(track["album"]["images"][0]["url"])
+    print()
 
