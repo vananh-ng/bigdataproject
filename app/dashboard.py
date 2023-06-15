b3c1897bfa36e43b6d95e7ed9b2523f46d4715dd
TimothyKusuma <s_kusuma@stud.hwr-berlin.de>
2023-06-15 16:56:33 +0200

    new changes

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,5 +1,60 @@
 import pandas as pd
 import pickle
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_pickle('app/data/album_and_artists.pkl')
-print(df.shape)
+
+# Create a function to make the genre graph
+def top_genres(df):
+    genres = df['genres'].value_counts().head(10)
+    plt.figure(figsize=(10,6))
+    sns.barplot(x=genres.values, y=genres.index, palette='viridis')
+    plt.title('Top Genres')
+    plt.xlabel('Count')
+    plt.ylabel('Genre')
+    st.pyplot(plt.clf())
+
+# Create a function to make the popular artists graph
+def popular_artists(df):
+    popular_artists = df.sort_values('followers', ascending=False)['artists_name'].head(10)
+    plt.figure(figsize=(10,6))
+    sns.barplot(x=popular_artists.values, y=popular_artists.index, palette='viridis')
+    plt.title('Popular Artists')
+    plt.xlabel('Followers')
+    plt.ylabel('Artist')
+    st.pyplot(plt.clf())
+
+# Create a function to make the artist popularity over time graph
+def popularity_over_time(df, artist_name):
+    artist_df = df[df['artists_name'] == artist_name]
+    plt.figure(figsize=(10,6))
+    sns.lineplot(x=artist_df['release_year'], y=artist_df['artist_popularity'], palette='viridis')
+    plt.title(f'{artist_name} Popularity Over Time')
+    plt.xlabel('Year')
+    plt.ylabel('Popularity')
+    st.pyplot(plt.clf())
+
+# Create a function to make the album types graph
+def album_types(df):
+    album_types = df['album_type'].value_counts()
+    plt.figure(figsize=(10,6))
+    plt.pie(album_types.values, labels=album_types.index, autopct='%1.1f%%', startangle=140)
+    plt.title('Album Types')
+    st.pyplot(plt.clf())
+
+# Layout of Streamlit app
+st.title('Spotify Dashboard')
+if st.checkbox('Show Top Genres'):
+    top_genres(df)
+
+if st.checkbox('Show Popular Artists'):
+    popular_artists(df)
+
+artist_name = st.text_input('Enter an artist name to see their popularity over time:')
+if artist_name:
+    popularity_over_time(df, artist_name)
+
+if st.checkbox('Show Album Types'):
+    album_types(df)
--- app/preprocessing.py
+++ app/preprocessing.py
@@ -19,15 +19,28 @@ df_artist = df_artist.dropna(subset=['genres'])
 df_artist = df_artist[df_artist['genres'].apply(lambda x: ast.literal_eval(x) != [])]
 df_artist = df_artist.dropna(subset=['followers'])
 
+def extract_year(date_str):
+    if "/" in date_str:
+        return date_str.split("/")[2] # if format is "12/1/1981", year is the 3rd part
+    elif "-" in date_str:
+        return date_str.split("-")[0] # if format is "1981-08", year is the 1st part
+    else:
+        return date_str # return as is
+
+# convert the release_date column to year only
+df_album['release_date'] = df_album['release_date'].apply(extract_year)
+df_album['release_date'] = df_album['release_date'].astype(int)
+
 # drop unusable columns
-df_album = df_album.drop(['Unnamed: 0', 'available_markets', 'type', 'uri', 'external_urls', 'href', 'images', 'track_name_prev', 'track_id'], axis=1)
+df_album = df_album.drop(['Unnamed: 0', 'available_markets', 'type', 'uri', 'external_urls', 'href', 'images', 'track_name_prev', 'track_id', 'release_date_precision'], axis=1)
 df_artist = df_artist.drop(['Unnamed: 0', 'type', 'track_name_prev', 'track_id'], axis=1)
 
 # merge the two dataframes
 df_merge = pd.merge(df_album, df_artist, left_on='artist_id', right_on='artists_id', how='inner')
 df_merge = df_merge.drop(['artists_id'], axis=1)
-df_merge = df_merge.rename(columns={'id': 'album_id'})
+df_merge = df_merge.rename(columns={'id': 'album_id', 'release_date': 'release_year'})
 
 # save the merged dataframe
 with open('app/data/album_and_artists.pkl', 'wb') as file:
     pickle.dump(df_merge, file)
+    print("Dataframe saved to pickle file.")
