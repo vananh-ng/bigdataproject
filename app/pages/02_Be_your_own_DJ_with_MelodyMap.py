1e405882e705c6d13045944f027ea13687a98964
vananh-ng <94485420+vananh-ng@users.noreply.github.com>
2023-06-23 00:22:41 +0200

    test

--- app/pages/02_Be_your_own_DJ_with_MelodyMap.py
+++ app/pages/02_Be_your_own_DJ_with_MelodyMap.py
@@ -65,7 +65,8 @@ def run_model(system_message, user_message):
 system_message = "As a Spotify playlist recommender, \
     your task is to provide song recommendations based on users' description of their current mood.\
     You should aim to suggest a maximum of 10 songs that align with their request.\
-    Your tone is fun, compassion and friendly."
+    Your tone is fun, compassion and friendly. Your goal is to make the user feel understood and happy.\
+    Your response should be in the form of a Spotify URL link."
 user_message = st.text_input("How's your mood today?")
 if st.button("Send"):
     response = run_model(system_message, user_message)
