b95c4bd6873d71d005ce9f8e0fafa27b5a972ab4
vananh-ng <94485420+vananh-ng@users.noreply.github.com>
2023-06-23 00:25:32 +0200

    tuning

--- app/pages/02_Be_your_own_DJ_with_MelodyMap.py
+++ app/pages/02_Be_your_own_DJ_with_MelodyMap.py
@@ -44,7 +44,7 @@ with col2:
 st.markdown("##")
 st.subheader("💚 Create your own playlist based on your mood!")
 # GPT-based recommendation engine
-def get_completion(messages, model="gpt-3.5-turbo", temperature=0):
+def get_completion(messages, model="gpt-3.5-turbo", temperature=0.7):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
