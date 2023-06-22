c593391d0f4096b0418b008c3271f157d0ba8afc
Sofía Inés Stephens Fracassi <122200446+SofiStephens@users.noreply.github.com>
2023-06-22 15:32:38 +0200

    sort countries

--- app/pages/03_New_Releases_World_Map.py
+++ app/pages/03_New_Releases_World_Map.py
@@ -54,7 +54,7 @@ def get_auth_header(token):
 
 # Read country data
 df_countries = pd.read_pickle('app/data/new_releases.pkl')
-
+df_countries= df_countries.sort_values(by= 'country')
 
 # Plot the map
 fig = px.scatter_geo(df_countries, lat='cap_lat', lon= 'cap_lon',
@@ -129,4 +129,4 @@ selected_artist= df_countries.loc[df_countries['country']== country,'artist'].va
 st.sidebar.write(f"The latest Album in **{country}** is **{selected_album}** by **{selected_artist}**")
 st.sidebar.write('Are you curious about its tracks? Check them out!🎧')
 st.sidebar.dataframe(selected_tracks, hide_index=True, use_container_width= True)
-st.sidebar.write("If you want to listen to it, go to the /'Be your own DJ with MelodyMap' tab and search for the song!")
+st.sidebar.write("If you want to listen to it, go to the 'Be your own DJ with MelodyMap' tab and search for the song!")
