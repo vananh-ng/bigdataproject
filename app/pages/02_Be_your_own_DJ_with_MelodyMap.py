4eeb29cba9e5fd83eb57d25d34c842fc5325d74d
vananh-ng <94485420+vananh-ng@users.noreply.github.com>
2023-07-05 16:35:26 +0200

    update

--- app/pages/02_Be_your_own_DJ_with_MelodyMap.py
+++ app/pages/02_Be_your_own_DJ_with_MelodyMap.py
@@ -148,7 +148,7 @@ else:
 
     # Create the Spotify embed in the sidebar
     st.sidebar.markdown(
-        f'<iframe src="https://open.spotify.com/embed/track/{song_info["uri"]}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>',
+        f'<iframe src="https://open.spotify.com/embed/track/{song_info["uri"]}" width="200" height="280" frameborder="0" allowtransparency="true" ></iframe>',
         unsafe_allow_html=True,
     )
 
