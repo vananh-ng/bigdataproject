4b72b40e3bf883bcbfa9a3a8d799d0defcfe82b2
Sofía Inés Stephens Fracassi <122200446+SofiStephens@users.noreply.github.com>
2023-08-12 20:55:20 +0200

    update

--- app/update_new_releases.py
+++ app/update_new_releases.py
@@ -8,11 +8,9 @@ import pandas as pd
 from dotenv import load_dotenv
 import plotly.express as px
 import pickle
-import schedule
-import time
 
-'''This code is to update the new_releases dataframe with the data from the API:
-    It's scheudled to run every 3 days at 00:00 and replace the existing dataframe'''
+
+'''This code is to create the initial new_releases dataframe and to update it with the data from the API'''
 
 # Spotify Developer Dashboard details
 load_dotenv()
@@ -49,41 +47,40 @@ token = get_token()
 def get_auth_header(token):
     return{"Authorization": "Bearer " + token}
 
-#query= {what I want to get}&type= artist, track, playlist, album, artist, playlist, track, show, episode, audiobook.&limit=1 (first artist that pops up, most popular artist)
 
 # Get new releases
-def get_new_releases(country):
+def get_new_releases(df, country):
     # Find the country code for the given country name
-    row = df_countries[df_countries['country'] == country]
+    row = df[df['country'] == country]
     country_code = row.iloc[0]['country_code']
     url= f"https://api.spotify.com/v1/browse/new-releases?country={country_code}&limit=1"
     headers = get_auth_header(token)
     result = get(url, headers=headers)
+    # Get the album name
     json_result = json.loads(result.content)['albums']['items'][0]['name']
     return json_result
 
-# Get artist new releases
-def get_artist_new_releases(country):
+# Get the artist of the new releases
+def get_artist_new_releases(df,country):
     # Find the country code for the given country name
-    row = df_countries[df_countries['country'] == country]
+    row = df[df['country'] == country]
     country_code = row.iloc[0]['country_code']
     url= f"https://api.spotify.com/v1/browse/new-releases?country={country_code}&limit=1"
     headers = get_auth_header(token)
     result = get(url, headers=headers)
+    # Get the artist name
     json_result = json.loads(result.content)['albums']['items'][0]['artists'][0]['name']
     return json_result
 
-
-# Read country data
-df_countries= pd.read_excel(r'C:\Users\sofia\OneDrive\Documentos\GitHub\bigdataproject\app\data\country-available-final.xlsx')
-df_countries.iloc[115,4]='NA' #Modify for Namibia: it detects NaN instead of NA country code
+# Create the initial dataset
+#df_countries= pd.read_excel(r"app\data\country-available-final.xlsx")
+#df_countries.iloc[115,4]='NA' #Modify for Namibia: it detects NaN instead of NA country code
 
 # Get the new releases per country
 #df_countries['new_releases'] = [get_new_releases(df_countries.loc[index, 'country']) for index in range(len(df_countries))]
 
 # Get the new release artist
 #df_countries['artist'] = [get_artist_new_releases(df_countries.loc[index, 'country']) for index in range(len(df_countries))]
-
 
 # Function to update new releases in the dataframe
 def update_new_releases(pickle_file_path):
@@ -95,10 +92,10 @@ def update_new_releases(pickle_file_path):
     df_countries.iloc[115,4]='NA' 
         
     # Get the new releases per country
-    df_countries['new_releases'] = [get_new_releases(df_countries.loc[index, 'country']) for index in range(len(df_countries))]
+    df_countries['new_releases'] = [get_new_releases(df_countries, df_countries.loc[index, 'country']) for index in range(len(df_countries))]
 
     # Get the new release artist
-    df_countries['artist'] = [get_artist_new_releases(df_countries.loc[index, 'country']) for index in range(len(df_countries))]
+    df_countries['artist'] = [get_artist_new_releases(df_countries, df_countries.loc[index, 'country']) for index in range(len(df_countries))]
 
      # Save the updated dataframe to a temporary pickle file
     temp_pickle_file_path = os.path.splitext(pickle_file_path)[0] + '_temp.pkl'
@@ -109,9 +106,7 @@ def update_new_releases(pickle_file_path):
     os.replace(temp_pickle_file_path, pickle_file_path)
     print("New releases updated successfully!")
 
-pickle_file_path= r"/data/new_releases.pkl"
-schedule.every(3).days.at("00:00").do(update_new_releases, pickle_file_path)
 
-while True: # This starts an infinite loop.
-    schedule.run_pending() # This line checks if there are any scheduled tasks that are due to run and executes them. If there are no pending tasks, it does nothing.
-    time.sleep(1) #After checking for pending tasks, the program pauses for 1 second using the time.sleep() function. This ensures that the loop doesn't consume excessive resources and provides a delay between iterations.
+
+pickle_file_path= r"/data/new_releases.pkl"
+update_new_releases(pickle_file_path)
